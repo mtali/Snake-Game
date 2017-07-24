58aa58f4ecf910c2566b06ca925101500bed0461
mtali <mtali.emmanuel@protonmail.com>
2017-07-24 21:36:14 +0300

    Render snake head

--- core/src/com/colisa/snakegame/GameScreen.java
+++ core/src/com/colisa/snakegame/GameScreen.java
@@ -5,24 +5,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
     private SpriteBatch batch;
+    private Texture snakeHead;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
+        snakeHead = new Texture("snakehead.png");
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(
+                snakeHead,
+                0,
+                0
+        );
+        batch.end();
     }
 
     @Override
     public void dispose() {
+        snakeHead.dispose();
         batch.dispose();
     }
 }
