4d8b4f32e7e183767cbbd01ec8b8aa32f3fe37c1
mtali <mtali.emmanuel@protonmail.com>
2017-07-24 22:34:30 +0300

    Generate apple once randomly

--- core/src/com/colisa/snakegame/GameScreen.java
+++ core/src/com/colisa/snakegame/GameScreen.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+import com.badlogic.gdx.math.MathUtils;
 
 public class GameScreen extends ScreenAdapter {
     private static final float MOVE_TIME = 1f;
@@ -26,28 +26,43 @@ public class GameScreen extends ScreenAdapter {
 
     private SpriteBatch batch;
     private Texture snakeHead;
+    private Texture apple;
+    private boolean appleAvailable = false;
+    private int appleX, appleY;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         snakeHead = new Texture("snakehead.png");
+        apple = new Texture("apple.png");
     }
 
     @Override
     public void render(float delta) {
         queryInput();
+
         timer -= delta;
         if (timer < 0) {
             timer = MOVE_TIME;
             moveSnake();
             checkForOutOfBounds();
         }
+        checkAndPlaceApple();
+        clearScreen();
+        draw();
 
+    }
 
+    private void clearScreen(){
         Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
 
+    private void draw(){
         batch.begin();
+        if (appleAvailable)
+            batch.draw(apple, appleX, appleY);
+
         batch.draw(
                 snakeHead,
                 snakeX,
@@ -57,7 +72,7 @@ public class GameScreen extends ScreenAdapter {
         batch.end();
     }
 
-    private void checkForOutOfBounds(){
+    private void checkForOutOfBounds() {
         if (snakeX >= Gdx.graphics.getWidth())
             snakeX = 0;
 
@@ -72,7 +87,7 @@ public class GameScreen extends ScreenAdapter {
 
     }
 
-    private void moveSnake(){
+    private void moveSnake() {
         switch (snakeDirection) {
             case RIGHT:
                 snakeX += SNAKE_MOVEMENT;
@@ -89,15 +104,26 @@ public class GameScreen extends ScreenAdapter {
         }
     }
 
-    private void queryInput(){
+    private void queryInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) snakeDirection = LEFT;
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) snakeDirection = RIGHT;
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) snakeDirection = UP;
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) snakeDirection = DOWN;
     }
 
+    private void checkAndPlaceApple() {
+        if (!appleAvailable){
+            do {
+                appleX = MathUtils.random(Gdx.graphics.getWidth() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+                appleY = MathUtils.random(Gdx.graphics.getHeight() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+                appleAvailable = true;
+            }while (appleX == snakeX && appleY == snakeY);
+        }
+    }
+
     @Override
     public void dispose() {
+        apple.dispose();
         snakeHead.dispose();
         batch.dispose();
     }
