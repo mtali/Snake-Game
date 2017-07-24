ffc78656f03397eac159a395a1b340e9013d47f8
mtali <mtali.emmanuel@protonmail.com>
2017-07-24 23:43:00 +0300

    Increase snake length when colliding with an apple

--- core/src/com/colisa/snakegame/GameScreen.java
+++ core/src/com/colisa/snakegame/GameScreen.java
@@ -7,8 +7,10 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 
 public class GameScreen extends ScreenAdapter {
     private static final float MOVE_TIME = 1f;
@@ -29,12 +31,19 @@ public class GameScreen extends ScreenAdapter {
     private Texture apple;
     private boolean appleAvailable = false;
     private int appleX, appleY;
+    private Texture snakeBody;
+    private Array<BodyPart> bodyParts;
+    private int snakeXBeforeUpdate = 0, snakeYBeforeUpdate = 0;
+
 
     @Override
     public void show() {
         batch = new SpriteBatch();
+        bodyParts = new Array<BodyPart>();
+
         snakeHead = new Texture("snakehead.png");
         apple = new Texture("apple.png");
+        snakeBody = new Texture("snakebody.png");
     }
 
     @Override
@@ -46,22 +55,23 @@ public class GameScreen extends ScreenAdapter {
             timer = MOVE_TIME;
             moveSnake();
             checkForOutOfBounds();
+            updateBodyPartsPosition();
         }
+        checkAppleCollision();
         checkAndPlaceApple();
+
         clearScreen();
         draw();
 
     }
 
-    private void clearScreen(){
+    private void clearScreen() {
         Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
-    private void draw(){
+    private void draw() {
         batch.begin();
-        if (appleAvailable)
-            batch.draw(apple, appleX, appleY);
 
         batch.draw(
                 snakeHead,
@@ -69,7 +79,23 @@ public class GameScreen extends ScreenAdapter {
                 snakeY
         );
 
+        for (BodyPart bodyPart : bodyParts) {
+            bodyPart.draw(batch);
+        }
+
+        if (appleAvailable)
+            batch.draw(apple, appleX, appleY);
+
         batch.end();
+    }
+
+    private void checkAppleCollision() {
+        if (appleAvailable && snakeX == appleX && snakeY == appleY) {
+            BodyPart bodyPart = new BodyPart(snakeBody);
+            bodyPart.updateBodyPart(snakeX, snakeY);
+            bodyParts.insert(0, bodyPart);
+            appleAvailable = false;
+        }
     }
 
     private void checkForOutOfBounds() {
@@ -88,6 +114,9 @@ public class GameScreen extends ScreenAdapter {
     }
 
     private void moveSnake() {
+        snakeXBeforeUpdate = snakeX;
+        snakeYBeforeUpdate = snakeY;
+
         switch (snakeDirection) {
             case RIGHT:
                 snakeX += SNAKE_MOVEMENT;
@@ -104,6 +133,14 @@ public class GameScreen extends ScreenAdapter {
         }
     }
 
+    private void updateBodyPartsPosition() {
+        if (bodyParts.size > 0) {
+            BodyPart bodyPart = bodyParts.removeIndex(0);
+            bodyPart.updateBodyPart(snakeXBeforeUpdate, snakeYBeforeUpdate);
+            bodyParts.add(bodyPart);
+        }
+    }
+
     private void queryInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) snakeDirection = LEFT;
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) snakeDirection = RIGHT;
@@ -112,19 +149,38 @@ public class GameScreen extends ScreenAdapter {
     }
 
     private void checkAndPlaceApple() {
-        if (!appleAvailable){
+        if (!appleAvailable) {
             do {
                 appleX = MathUtils.random(Gdx.graphics.getWidth() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
                 appleY = MathUtils.random(Gdx.graphics.getHeight() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
                 appleAvailable = true;
-            }while (appleX == snakeX && appleY == snakeY);
+            } while (appleX == snakeX && appleY == snakeY);
         }
     }
 
     @Override
     public void dispose() {
+        snakeBody.dispose();
         apple.dispose();
         snakeHead.dispose();
         batch.dispose();
+    }
+
+    private class BodyPart {
+        private int x, y;
+        private Texture texture;
+
+        public BodyPart(Texture texture) {
+            this.texture = texture;
+        }
+
+        public void updateBodyPart(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void draw(Batch batch) {
+            if (!(x == snakeX && y == snakeY)) batch.draw(texture, x, y);
+        }
     }
 }
