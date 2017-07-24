88b92c5d34cd4e11ce69e9adc8cbb25c41c93565
mtali <mtali.emmanuel@protonmail.com>
2017-07-24 21:16:09 +0300

    Initial commit

---
+++ html/src/com/colisa/snakegame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.colisa.snakegame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.colisa.snakegame.SnakeGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SnakeGame();
+        }
+}
